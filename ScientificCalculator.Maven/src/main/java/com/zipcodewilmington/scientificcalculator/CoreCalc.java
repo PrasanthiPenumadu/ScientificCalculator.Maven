4bc24afa1123effe46334caec50b4ad9ca9e1a1c
HFCatania <50722490+HFCatania@users.noreply.github.com>
2019-05-26 17:17:34 -0400

    Added core calculator functions.
    Added core calculator functions'
    Added basic/scientific calculator selector 
    Need to add error detection 
    Need to add clear function
    Need to add extra functions

---
+++ ScientificCalculator.Maven/src/main/java/com/zipcodewilmington/scientificcalculator/CoreCalc.java
@@ -0,0 +1,36 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class CoreCalc {
+    public double basicCalcs (double d, double d2, String s){
+
+        Double result= 0.0;
+        switch (s) {
+            case "+": result = d + d2;
+                break;
+            case "-": result = d - d2;
+                break;
+            case "*": result = d * d2;
+                break;
+            case "/": result = d / d2;
+                break;
+        }
+        return result;
+    }
+
+    public double advancedCalcs (String s,Double d, Double d2){
+
+        Double result = 0.0;
+        switch (s) {
+            case "square root": result = Math.sqrt(d);
+                break;
+            case "square": result = Math.pow(d, 2);
+                break;
+            case "exponentiation": result = Math.pow(d, d2);
+                break;
+            case "invert": result = d * -d;
+                break;
+            case "reciprocal": result = 1.0/d;
+        }
+        return result;
+    }
+}
