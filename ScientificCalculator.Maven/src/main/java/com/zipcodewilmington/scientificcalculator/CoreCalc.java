b03b697ded41215d930b5f969b9a8f908af776e8
HFCatania <50722490+HFCatania@users.noreply.github.com>
2019-05-26 18:04:04 -0400

    revised the functions of corecalc.
    Can't figure out how to stop it from asking for a second double on advanceCalcs2 methods

--- ScientificCalculator.Maven/src/main/java/com/zipcodewilmington/scientificcalculator/CoreCalc.java
+++ ScientificCalculator.Maven/src/main/java/com/zipcodewilmington/scientificcalculator/CoreCalc.java
@@ -1,7 +1,7 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class CoreCalc {
-    public double basicCalcs (double d, double d2, String s){
+    public double basicCalcs (String s, double d, double d2){
 
         Double result= 0.0;
         switch (s) {
@@ -21,11 +21,19 @@ public class CoreCalc {
 
         Double result = 0.0;
         switch (s) {
+            case "exponentiation": result = Math.pow(d, d2);
+                break;
+
+        }
+        return result;
+    }
+
+    public double advancedCalcs2 (String s, Double d){
+        Double result =0.0;
+        switch (s){
             case "square root": result = Math.sqrt(d);
                 break;
             case "square": result = Math.pow(d, 2);
-                break;
-            case "exponentiation": result = Math.pow(d, d2);
                 break;
             case "invert": result = d * -d;
                 break;
