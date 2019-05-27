f79fac74d1d7576e00ea5b8af0934ccfe286ecfa
PrasanthiPenumadu <50220476+PrasanthiPenumadu@users.noreply.github.com>
2019-05-27 15:54:46 -0400

    Update MainApplication.java

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -6,31 +6,35 @@ package com.zipcodewilmington.scientificcalculator;
 public class MainApplication {
     public static void main(String[] args) {
         Console.println("Welcome to our calculator!");
-        String welcome = Console.getStringInput("Please enter 'C'Core Calculator or'S'Scientific Calculator");
-        if (welcome.equalsIgnoreCase("S")) {
-            String s = Console.getStringInput("Enter the required mathematical operation : ").toLowerCase();
-            ScientificCalc scientific = new ScientificCalc();
-            while (!s.equalsIgnoreCase("Exit")) {
-                //  Integer i = Console.getIntegerInput("Enter an integer value as operand");
-                Double d = Console.getDoubleInput("Enter a double value as the number");
-
-                Console.println("The user input %s as the required operation ", s);
-                // Console.println("The user input %s as an integer", i);
-                Console.println("The user input %s as a input number ", d);
-                System.out.println("The computed value of the operation is " + scientific.MathOperation(s, d));
-
-                s = Console.getStringInput("Enter another mathematical operation or 'Exit' to Quit!").toLowerCase();
+        String s = Console.getStringInput("Please enter 'C'Core Calculator or'S'Scientific Calculator");
+        while (!s.equalsIgnoreCase(("exit"))) {
+            while (s.equalsIgnoreCase("S")) {
+                ScientificCalc scientific = new ScientificCalc();
+                s = Console.getStringInput("Enter the required mathematical operation : ").toLowerCase();
+                while (!s.equalsIgnoreCase("c") && !s.equalsIgnoreCase("exit")) {
+                    if (!(s.equalsIgnoreCase("mc") || s.equalsIgnoreCase("mrc"))) {
+                        Double d = Console.getDoubleInput("Enter a double value as the operand");
+                        Console.println("The user input %s as the required operation ", s);
+                        Console.println("The user input %s as a input operand ", d);
+                        System.out.println("The " + s + " of " + d + " is " + scientific.MathOperation(s, d));
+                    }
+                    if (s.equalsIgnoreCase("mrc"))
+                        System.out.println("The value stored in calculator is " + scientific.MathOperation(s));
+                    if (s.equalsIgnoreCase("mc"))
+                        System.out.println("The value stored in calculator is set to " + scientific.MathOperation(s));
+                    s = Console.getStringInput("Enter another mathematical operation or 'Exit' to Quit or 'C' to switch to Core Calculator!").toLowerCase();
+                }
             }
-            System.out.println("Thank you for using our calculator, Enjoy the rest of your day");
-        } else {
+        while (s.equalsIgnoreCase("C")) {
             CoreCalc core = new CoreCalc();
             String s = Console.getStringInput("Enter an operation").toLowerCase();
-            while (!s.equalsIgnoreCase("Exit")) {
+             while(!(s.equalsIgnoreCase("s"))&&!(s.equalsIgnoreCase("exit"))) {
+                   
                 if (s.equalsIgnoreCase("random") || s.equalsIgnoreCase("pi")){
                     System.out.println("The computed value of the operation is " + core.bonusCalcs(s));
                 }
 
-                //Console.println("The user input %s as a d", d2);
+        
                 if (s.equalsIgnoreCase("square root") || s.equalsIgnoreCase("square") || s.equalsIgnoreCase("invert")) {
                     Double d = Console.getDoubleInput("Enter the first number");
                     System.out.println("The computed value of the operation is " + core.advancedCalcs2(s, d));
@@ -51,8 +55,10 @@ public class MainApplication {
 
                 s = Console.getStringInput("Enter another mathematical operation or 'Exit' to Quit!").toLowerCase();
             }
-            System.out.println("Thank you for using our calculator, Enjoy the rest of your day");
         }
+    }
+            System.out.println("Thank you for using our calculator, Enjoy the rest of your day");
+        
     }
 }
 
